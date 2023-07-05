b550d45ae4e045c9cafd9145f45db9dfd932dd2a
HugoLaurent <lauhugopro@gmail.com>
2023-07-05 14:41:53 +0200

    add: dotenv set up

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const routerPublic = require("./router/routerPublic");
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 // Définissez vos routes et middleware ici
 app.use(express.json());
@@ -9,8 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routerPublic);
 
-// Démarrez le serveur
-const port = 3000; // Définissez le numéro de port que vous souhaitez utiliser
-app.listen(port, () => {
-  console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
+// Démarrez le serveur // Définissez le numéro de port que vous souhaitez utiliser
+app.listen(PORT, () => {
+  console.log(`Le serveur est en cours d'exécution sur le port ${PORT}`);
 });
--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("libranga", "libranga", "libranga", {
-  host: "localhost",
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
   dialect: "postgres",
   port: 5432,
   define: {
