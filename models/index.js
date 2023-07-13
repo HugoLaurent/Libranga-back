0b3f62971a0087b533a899f1d46d3211c3012ff8
HugoLaurent <lauhugopro@gmail.com>
2023-07-13 19:59:08 +0200

    add: category and article relation

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Article = require("./Article");
 const Comment = require("./Comment");
+const Category = require("./Category");
 
 User.hasMany(Article, { foreignKey: "user_id" });
 Article.belongsTo(User, { foreignKey: "user_id" });
@@ -10,5 +11,8 @@ Comment.belongsTo(Article, { foreignKey: "article_id" });
 
 User.hasMany(Comment, { foreignKey: "user_id" });
 Comment.belongsTo(User, { foreignKey: "user_id" });
+
+Category.hasMany(Article, { foreignKey: "category_id" });
+Article.belongsTo(Category, { foreignKey: "category_id" });
 
 module.exports = { User, Article };
