0d88b7e9074e6c5c0519cb0642726f948fb54132
HugoLaurent <lauhugopro@gmail.com>
2023-07-22 04:50:23 +0200

    Fix: for deploy

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
 
-const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbName = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
 
-const sequelize = new Sequelize("libranga", "libranga", "libranga", {
-  host: "localhost",
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "postgres",
   port: 5432,
   define: {
